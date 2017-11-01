127e309655d3b9d31319e67d2e0261e86cc9c35b
RochelLevi <rochlevi@gmail.com>
2017-11-01 15:36:08 -0400

    started editing person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+class Person
+
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness
+    if  @happiness > 9
+      raise "Please enter a number between 0 and 10"
+    else
+      @happiness += 1
+    end
+
+  end
+
+
+end
