1f59b62a8c617432970be7f89d641e081cb5aedc
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-01 17:22:03 -0400

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Person
 
   attr_reader :name, :happiness, :hygiene
@@ -10,14 +12,72 @@ class Person
     @hygiene = 8
   end
 
-  def happiness
-    if  @happiness > 9
-      raise "Please enter a number between 0 and 10"
+  def happiness=(num)
+    if  num > 10
+      @happiness=10
+    elsif num<0
+      @happiness=0
     else
-      @happiness += 1
+      @happiness = num
     end
 
   end
+
+  def hygiene=(num)
+    if  num > 10
+      @hygiene=10
+    elsif num<0
+      @hygiene=0
+    else
+      @hygiene = num
+    end
+  end
+
+    def happy?
+      return true if @happiness>7
+      false
+    end
+
+    def clean?
+      return true if @hygiene>7
+      false
+    end
+
+    def get_paid(salary)
+      @bank_account+=salary
+      return "all about the benjamins"
+    end
+
+    def take_bath
+      self.hygiene+=4
+      return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+      self.happiness+=2
+      self.hygiene-=3
+      return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+      self.happiness+=3
+      friend.happiness+=3
+      return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+    def start_conversation(person, topic)
+      if topic=="politics"
+        self.happiness-=2
+        person.happiness-=2
+        return "blah blah partisan blah lobbyist"
+      elsif topic=="weather"
+        self.happiness+=1
+        person.happiness+=1
+        return "blah blah sun blah rain"
+      else
+        return "blah blah blah blah blah"
+      end
+    end
+
 
 
 end
